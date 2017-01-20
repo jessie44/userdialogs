45178be4bec0d8aa2faa729a385a24896ef0fc6f
Allan Ritchie <allan.ritchie@gmail.com>
2017-01-20 11:43:30 -0500

    Moving in tvOS & macOS

---
+++ src/Acr.UserDialogs.Mac/UserDialogsImpl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Splat;
+
+
+namespace Acr.UserDialogs
+{
+    public class UserDialogsImpl : AbstractUserDialogs
+    {
+        protected override IProgressDialog CreateDialogInstance(ProgressDialogConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override IAlertDialog CreateDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override IDisposable DatePrompt(DatePromptConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override IDisposable TimePrompt(TimePromptConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override IDisposable Toast(ToastConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override void ShowImage(IBitmap image, string message, int timeoutMillis)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
